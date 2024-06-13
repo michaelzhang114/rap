3536247a703c4c9e91e126df2c20c0744a3da690
michaelzhang114 <michaelzhang6284@gmail.com>
2024-06-12 21:31:43 -0700

    login works

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
 	return (
 		<html lang="en">
-			<body>
+			<body className="box-border">
 				<Provider>
 					<main className="app">
 						<Nav />
