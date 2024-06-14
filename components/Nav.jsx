ccba0a07d24952ce2bfe9a5ca2bef9184822b787
michaelzhang114 <michaelzhang6284@gmail.com>
2024-06-14 00:40:22 -0700

    allow google image on next config

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -50,10 +50,13 @@ const Nav = () => {
 								className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
 							>
 								<li>
-									<a className="justify-between">
+									<Link
+										className="justify-between"
+										href="/profile"
+									>
 										Profile
 										<span className="badge">New</span>
-									</a>
+									</Link>
 								</li>
 								<li>
 									{/* <a>Generate Verse</a> */}
