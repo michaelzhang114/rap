90ac3c9a8f5fb8c87f4d4de6536ef684997e6ad1
michaelzhang114 <michaelzhang6284@gmail.com>
2024-07-13 14:43:19 -0700

    first

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Hero from "@components/Hero";
 import Testimonials from "@components/Testimonials";
 import Link from "next/link";
 import React from "react";
+
 import { redirect } from "next/navigation";
 
 const Home = () => {
